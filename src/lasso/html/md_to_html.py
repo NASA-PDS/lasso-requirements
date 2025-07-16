c3786f2efb1043fda31c6e9bbfdde3ad8d302f75
Sean Kelly <kelly@seankelly.biz>
2025-07-16 15:32:35 -0500

    Missed a pkg_resources

--- src/lasso/html/md_to_html.py
+++ src/lasso/html/md_to_html.py
@@ -2,7 +2,7 @@
 import emoji
 from jinja2 import Template
 from markdown2 import Markdown
-from pkg_resources import resource_string
+import importlib.resources
 
 
 def md_to_html(in_file, out_file, template_kargs):
@@ -16,7 +16,7 @@ def md_to_html(in_file, out_file, template_kargs):
         html_emojized_str = emoji.emojize(html_str, use_aliases=True)
         template_kargs["requirements_html"] = html_emojized_str
 
-    template = Template(resource_string(__name__, "REQUIREMENTS.html.template").decode("utf-8"))
+    template = Template(importlib.resources.files(__name__).joinpath("REQUIREMENTS.html.template").read_text())
     with open(out_file, "w") as f_out:
         html_str = template.render(template_kargs)
         f_out.write(html_str)
